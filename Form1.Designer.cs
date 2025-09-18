1e4e0569184ea74a1bf8f3eccaa891f7807eb604
Murillo Carvalho <152340064+murillogyr@users.noreply.github.com>
2025-09-18 16:01:28 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ChatBasicoSetores
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Timer timer1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtAutor = new System.Windows.Forms.Label();
+            this.txtLogo = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnAbrirLog = new System.Windows.Forms.Button();
+            this.listMensagens = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.Location = new System.Drawing.Point(42, 798);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(951, 62);
+            this.txtMensagem.TabIndex = 1;
+            this.txtMensagem.TextChanged += new System.EventHandler(this.txtMensagem_TextChanged);
+            this.txtMensagem.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMensagem_KeyDown);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEnviar.Location = new System.Drawing.Point(1013, 798);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(205, 62);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAutor.AutoSize = true;
+            this.txtAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAutor.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtAutor.Location = new System.Drawing.Point(1482, 456);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(324, 29);
+            this.txtAutor.TabIndex = 3;
+            this.txtAutor.Text = "Powered By Murillo Carvalho";
+            this.txtAutor.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtLogo
+            // 
+            this.txtLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLogo.Font = new System.Drawing.Font("Bahnschrift Condensed", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogo.Location = new System.Drawing.Point(1463, 51);
+            this.txtLogo.Name = "txtLogo";
+            this.txtLogo.Size = new System.Drawing.Size(402, 379);
+            this.txtLogo.TabIndex = 4;
+            this.txtLogo.Text = "PREMIER\r\nDENTAL\r\nMIX";
+            this.txtLogo.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLimpar.Location = new System.Drawing.Point(1236, 798);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(209, 62);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnAbrirLog
+            // 
+            this.btnAbrirLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAbrirLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrirLog.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAbrirLog.Location = new System.Drawing.Point(1470, 518);
+            this.btnAbrirLog.Name = "btnAbrirLog";
+            this.btnAbrirLog.Size = new System.Drawing.Size(209, 55);
+            this.btnAbrirLog.TabIndex = 6;
+            this.btnAbrirLog.Text = "Log";
+            this.btnAbrirLog.UseVisualStyleBackColor = true;
+            this.btnAbrirLog.Click += new System.EventHandler(this.btnAbrirLog_Click);
+            // 
+            // listMensagens
+            // 
+            this.listMensagens.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listMensagens.BackColor = System.Drawing.SystemColors.Menu;
+            this.listMensagens.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listMensagens.FormattingEnabled = true;
+            this.listMensagens.ItemHeight = 46;
+            this.listMensagens.Location = new System.Drawing.Point(42, 35);
+            this.listMensagens.Name = "listMensagens";
+            this.listMensagens.Size = new System.Drawing.Size(1402, 740);
+            this.listMensagens.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.btnAbrirLog);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.txtLogo);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.listMensagens);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Premier Dental Mix Gerenciamento";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAutor;
+        private System.Windows.Forms.Label txtLogo;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnAbrirLog;
+        private System.Windows.Forms.ListBox listMensagens;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace ChatBasicoSetores
+{
+    public partial class Form1 : Form
+    {
+        // definir o IP do PC onde a pasta está compartilhada
+        private string pastaCompartilhada = @"\\endereço ipv4\pastaCompartilhada";
+        private string arquivoMensagens = "mensagens.txt";
+        private string caminhoArquivo => Path.Combine(pastaCompartilhada, arquivoMensagens);
+
+        // guarda as linhas que já foram exibidas
+        private int ultimaLinhaExibida = 0;
+
+        public Form1()
+        {
+            this.AutoScaleMode = AutoScaleMode.None; // trava a escala
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // config visual do txtMensagem
+            txtMensagem.Multiline = false;
+            txtMensagem.Font = new Font(txtMensagem.Font.FontFamily, 36);
+            txtMensagem.Height = 60;
+            txtMensagem.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+            // listBox ocupa todo espaço disponível
+            listMensagens.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+            // configura ListBox para OwnerDraw
+            listMensagens.DrawMode = DrawMode.OwnerDrawVariable;
+            listMensagens.MeasureItem += ListMensagens_MeasureItem;
+            listMensagens.DrawItem += ListMensagens_DrawItem;
+
+            // verifica se a pasta compartilhada existe
+            if (!Directory.Exists(pastaCompartilhada))
+                Directory.CreateDirectory(pastaCompartilhada);
+
+            // cria o arquivo de mensagens se não existir
+            if (!File.Exists(caminhoArquivo))
+                File.WriteAllText(caminhoArquivo, "");
+
+            AtualizarMensagens();
+            timer1.Interval = 2000; // 🔹 atualiza a cada 2 segundos
+            timer1.Start();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMensagem.Text)) return;
+
+            string msg = $"[{DateTime.Now:dd/MM/yyyy|HH:mm}] {Environment.MachineName}: {txtMensagem.Text}";
+
+            try
+            {
+                File.AppendAllText(caminhoArquivo, msg + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar mensagem: " + ex.Message);
+            }
+
+            txtMensagem.Clear();
+            CarregarMensagens();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CarregarMensagens();
+        }
+
+        private void CarregarMensagens()
+        {
+            try
+            {
+                if (!File.Exists(caminhoArquivo))
+                {
+                    listMensagens.Items.Clear();
+                    ultimaLinhaExibida = 0;
+                    return;
+                }
+
+                var linhas = File.ReadAllLines(caminhoArquivo);
+                var novasMensagens = linhas.Skip(ultimaLinhaExibida).ToArray();
+                if (novasMensagens.Length == 0) return;
+
+                listMensagens.BeginUpdate();
+                foreach (var l in novasMensagens)
+                    listMensagens.Items.Add(l);
+                if (listMensagens.Items.Count > 0)
+                    listMensagens.TopIndex = listMensagens.Items.Count - 1;
+                listMensagens.EndUpdate();
+
+                ultimaLinhaExibida = linhas.Length;
+            }
+            catch { }
+        }
+
+        private void AtualizarMensagens()
+        {
+            CarregarMensagens();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            if (listMensagens.SelectedIndex >= 0)
+            {
+                // remove apenas os itens selecionados
+                listMensagens.Items.RemoveAt(listMensagens.SelectedIndex);
+            }
+            else
+            {
+                // limpa todo o listBox
+                listMensagens.Items.Clear();
+            }
+        }
+
+        private void btnAbrirLog_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(pastaCompartilhada))
+                    Directory.CreateDirectory(pastaCompartilhada);
+
+                if (!File.Exists(caminhoArquivo))
+                    File.WriteAllText(caminhoArquivo, "");
+
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "notepad.exe",
+                    Arguments = $"\"{caminhoArquivo}\"",
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao abrir log: " + ex.Message);
+            }
+        }
+
+        private void txtMensagem_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                btnEnviar.PerformClick();
+            }
+        }
+
+        // eventos para OwnerDraw do listBox
+        private void ListMensagens_MeasureItem(object sender, MeasureItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+            string texto = listMensagens.Items[e.Index].ToString();
+            SizeF tamanho = e.Graphics.MeasureString(texto, listMensagens.Font, listMensagens.Width);
+            e.ItemHeight = (int)tamanho.Height + 4; // adiciona um padding
+        }
+
+        private void ListMensagens_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+            e.DrawBackground();
+            string texto = listMensagens.Items[e.Index].ToString();
+            using (Brush brush = new SolidBrush(e.ForeColor))
+            {
+                e.Graphics.DrawString(texto, e.Font, brush, e.Bounds);
+            }
+            e.DrawFocusRectangle();
+        }
+
+        // eventos vazios do Design
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void txtMensagem_TextChanged(object sender, EventArgs e) { }
+        private void By_Murillo_Carvalho_TextChanged(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label1_Click_1(object sender, EventArgs e) { }
+    }
+}
